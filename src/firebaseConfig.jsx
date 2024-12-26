7d9cf3cbc3bb7d5b81dde3422ec157d1273b7cbf
Carlos Eduardo Turina <carloseduardoturina@gmail.com>
2024-12-26 13:55:56 -0300

    Update firebaseConfig.jsx

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -9,16 +9,16 @@ import 'dotenv/config';
 
 
 
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: Process.env.FIREBASE_API_KEY,
+  authDomain: Process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: Process.env.FIREBASE_PROJECT_ID,
+  storageBucket: Process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Process.env.FIREBASE_APP_ID,
+  measurementId: Process.env.FIREBASE_MEASUREMENT_ID,
 };
+
 
 
 // Verifica se todas as variáveis de ambiente estão definidas
