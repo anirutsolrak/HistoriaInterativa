2db50b988395908111e3b47b2480a436ef941d9e
anirutsolrak <carloseduardoturina@gmail.com>
2024-12-26 14:23:19 -0300

    correção da importação do speedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { SpeedInsights } from "@vercel/speed-insights/react"; // Certifique-se de usar "/react" para aplicações React padrão
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <SpeedInsights />
+  </StrictMode>
+);
