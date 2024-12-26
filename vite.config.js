2767ddbb4fdb0fff34b65aa17698dab21062f3f8
Carlos Eduardo Turina <carloseduardoturina@gmail.com>
2024-12-26 14:12:58 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig, loadEnv } from 'vite' // Importe loadEnv
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => { // Receba o 'mode' como argumento
-  const env = loadEnv(mode, process.cwd(), ''); // Carregue as variáveis de ambiente
-
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': env, // Defina 'process.env' para as variáveis carregadas
-    },
-  };
-});
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
